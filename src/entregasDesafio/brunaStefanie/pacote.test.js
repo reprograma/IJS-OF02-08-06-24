43cd60151e137eec2066a0acb8741f64783e6ca8
Bruna <brunastefanie@alu.ufc.br>
2024-06-08 16:34:59 -0300

    Segundo commit

--- src/entregasDesafio/brunaStefanie/pacote.test.js
+++ src/entregasDesafio/brunaStefanie/pacote.test.js
@@ -1,17 +1,48 @@
-const {calcularValorPorHora} = require('../../dominio/calculadora/Hora/valorHora');
-const { DIAS_UTEIS_NO_MES, HORAS_POR_DIA } = require('../../dominio/calculadora/constantes/constantes');
+const { calcularPacote } = require('../../dominio/calculadora/Projeto/pacote');
 
-describe('calcularValorPorHora', () => {
-    test("calcular hora para renda 3000 reais", () => {
-        const retorno = calcularValorPorHora(3000);
-        const esperado = 18;
-        expect(esperado).toBe(retorno);
-        });
-    test('calcular hora para renda 5000 reais', () => {
-        const rendaMensal = 5000;
-        const horasTrabalhadasPorMes = DIAS_UTEIS_NO_MES * HORAS_POR_DIA;
-        const valorEsperado = Math.ceil(rendaMensal / horasTrabalhadasPorMes);
-         expect(calcularValorPorHora(rendaMensal)).toBe(valorEsperado);
+describe('calcularPacote', () => {
+  
+ test('pacote básico para até 50h de trabalho', () => {
+        const horas = calcularPacote(23);
+        const pacoteEsperado = 'pacote_basico';
+    
+        expect(horas).toBe(pacoteEsperado);
+ }); 
+  test('para até 200h de trabalho', () => {
+    const horas = 200;
+    const pacoteEsperado = 'pacote_premium';
+
+    expect(calcularPacote(horas)).toBe(pacoteEsperado);
   });
+
+  test('para mais de 200h de trabalho', () => {
+    const horas = 250;
+    const pacoteEsperado = 'pacote_premium';
+
+    expect(calcularPacote(horas)).toBe(pacoteEsperado);
+  });
+
+  test('para menos de 50h de trabalho', () => {
+    const horas = 49;
+    const pacoteEsperado = 'pacote_basico';
+
+    expect(calcularPacote(horas)).toBe(pacoteEsperado);
+  });
+
+  test('entre 51 e 100h de trabalho', () => {
+    const horas = 75;
+    const pacoteEsperado = 'pacote_intermediario';
+
+    expect(calcularPacote(horas)).toBe(pacoteEsperado);
+  });
+
+  test('entre 101 e 200h de trabalho', () => {
+    const horas = 150;
+    const pacoteEsperado = 'pacote_premium';
+
+    expect(calcularPacote(horas)).toBe(pacoteEsperado);
+  });
+
 });
+
 
