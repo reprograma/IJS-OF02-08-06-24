5d541586e664e861c56ae4627df33b88bf92e14e
elizapimentel <elizapimentel@hotmail.com>
2024-06-08 16:44:33 -0300

    add novos testes

--- src/entregasDesafio/eliza-pimentel/horasPorProjeto.test.js
+++ src/entregasDesafio/eliza-pimentel/horasPorProjeto.test.js
@@ -23,5 +23,29 @@ describe("Calcular horas de projeto", () => {
         const retornado = calcularHorasDeProjeto(listaDeFuncionalidades);
         const esperado = 56;
         expect(esperado).toBe(retornado);
-    })
+    });
+
+    // Teste para lista vazia
+    test("Calcular horas de projeto - Lista vazia", () => {
+        const listaDeFuncionalidades = [];
+        const retornado = calcularHorasDeProjeto(listaDeFuncionalidades);
+        const esperado = 0;
+        expect(esperado).toBe(retornado);
+    });
+
+    // Teste para verificar funcionalidade não existente
+    test("Calcular horas de projeto - Funcionalidade inexistente", () => {
+        const listaDeFuncionalidades = ['funcionalidade_inexistente'];
+        const retornado = calcularHorasDeProjeto(listaDeFuncionalidades);
+        const esperado = NaN; // O valor retornado é undefined, mas na soma ele se torna NaN
+        expect(retornado).toBeNaN();
+    });
+
+    // Teste para funcionalidades repetidas
+    test("Calcular horas de projeto - Funcionalidades repetidas", () => {
+        const listaDeFuncionalidades = ['setup', 'setup', 'setup'];
+        const retornado = calcularHorasDeProjeto(listaDeFuncionalidades);
+        const esperado = 24; // 8 + 8 + 8
+        expect(esperado).toBe(retornado);
+    });
 });
